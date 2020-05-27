745b7362c2886868b00b49656b88f936cac0609a
saurabh <saurabh956@gmail.com>
2020-05-27 23:19:14 +0530

    second commit

--- empapi/views.py
+++ empapi/views.py
@@ -1,32 +1,20 @@
-# import datetime
-# from django.utils import timezone
 import json
 import os
-# from django.db.models import Q
-# from django.conf import settings
-# from django.conf.urls.static import static
-# from django.core import serializers as DJangoSerializers
-# from django.core.files.storage import FileSystemStorage
-# from django.http import HttpResponse, JsonResponse
 from django.shortcuts import get_object_or_404, render
 from rest_framework import serializers as RestSerializers
 from rest_framework import status, viewsets
 from rest_framework.decorators import (action,   parser_classes)
-# from rest_framework.parsers import FileUploadParser, JSONParser
 from rest_framework.permissions import AllowAny
 from rest_framework.renderers import (JSONRenderer, StaticHTMLRenderer, TemplateHTMLRenderer)
 from rest_framework.response import Response
 from rest_framework.views import APIView
 import requests
-# import logging
 import employment_api.settings as app_settings
 from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse
-# from twilio.twiml.messaging_response import MessagingResponse
 from django.http import FileResponse
 import requests
 import tempfile
-# from urllib.request import urlopen, Request
 from django.core import files
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -36,6 +24,8 @@ from . import models
 from selenium import webdriver 
 from selenium.webdriver.chrome.options import Options
 chrome_options = Options()
+from .captcha import GetCaptcha
+
 
 class aadhaar(viewsets.ViewSet):
     queryset = models.SessionModel.objects.all()
@@ -43,8 +33,8 @@ class aadhaar(viewsets.ViewSet):
     def aadhaar_verification(self, request):
         chrome_options.add_argument("--headless")
         chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
-        uid_no = '244340560639'
-        getcaptcha = captcha.GetCaptcha()
+        uid_no = request.data['uid']
+        getcaptcha = GetCaptcha()
         driver = webdriver.Chrome(executable_path="empapi\chromedriver\chromedriver.exe", options=chrome_options)
         driver.get("https://resident.uidai.gov.in/verify")
         getcaptcha.download_captcha(driver)
@@ -56,5 +46,6 @@ class aadhaar(viewsets.ViewSet):
         captcha.send_keys(captcha_text)
         driver.find_element_by_xpath('//*[@id="submitButton"]').click()
         elements = driver.find_element_by_xpath('//*[@id="maincontent"]/div/div[1]/div[3]')
-        print(elements.text)
-        return Response(elements.text)
+        txt = json.loads(elements.text)
+        return Response(txt)
+        
